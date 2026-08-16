9547de66f3b1a696af36dd3224ebea6a39fd6e7a
benpaul2002 <benpaul2002@gmail.com>
2026-08-15 22:39:35 -0400

    added model attention, forward + misc

--- model.py
+++ model.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 import json
 from safetensors import safe_open
 import torch
+import torch.nn.functional as F
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 qwen0_5_snapshot_path = "/home/bnp24202/.cache/huggingface/hub/models--Qwen--Qwen2.5-0.5B-Instruct/snapshots/7ae557604adf67be50417f59c2c2f167def9a775/"
 
@@ -52,5 +55,72 @@ def rmsnorm(x, weight, eps):
     x = x.to(dtype)
     return x * weight
 
-load_config(qwen0_5_snapshot_path)
-load_weights(qwen0_5_snapshot_path)
+def rope(head_dim, seq_len, theta, device):
+    inv_freq = 1.0 / (theta ** (torch.arange(0, head_dim, 2, device=device).float() / head_dim))
+    pos = torch.arange(seq_len, device=device, dtype=torch.float32)
+    freqs = torch.outer(pos, inv_freq)
+    emb = torch.cat([freqs, freqs], dim=-1)
+    return emb.cos(), emb.sin()
+
+def rotate_half(x):
+    first, second = torch.chunk(x, 2, dim=-1)
+    return torch.cat([-second, first], dim=-1)
+
+def apply_rope(q, k, cos, sin):
+    cos = cos.unsqueeze(0).unsqueeze(0)
+    sin = sin.unsqueeze(0).unsqueeze(0)
+    dtype = q.dtype
+    q_out = (q*cos + rotate_half(q)*sin).to(dtype)
+    k_out = (k*cos + rotate_half(k)*sin).to(dtype)
+    return q_out, k_out
+
+def gqa_attention(x, weights, layer_idx, config, cos, sin):
+    q_w = weights[f"model.layers.{layer_idx}.self_attn.q_proj.weight"]
+    q_b = weights[f"model.layers.{layer_idx}.self_attn.q_proj.bias"]
+    k_w = weights[f"model.layers.{layer_idx}.self_attn.k_proj.weight"]
+    k_b = weights[f"model.layers.{layer_idx}.self_attn.k_proj.bias"]
+    v_w = weights[f"model.layers.{layer_idx}.self_attn.v_proj.weight"]
+    v_b = weights[f"model.layers.{layer_idx}.self_attn.v_proj.bias"]
+    o_w = weights[f"model.layers.{layer_idx}.self_attn.o_proj.weight"]
+    q = F.linear(x, q_w, q_b)
+    k = F.linear(x, k_w, k_b)
+    v = F.linear(x, v_w, v_b)
+    B, S, _ = x.shape
+    n_heads = config.num_attention_heads
+    n_kv_heads = config.num_key_value_heads
+    head_dim = config.head_dim
+    q = q.view(B, S, n_heads, head_dim).transpose(1, 2)
+    k = k.view(B, S, n_kv_heads, head_dim).transpose(1, 2)
+    v = v.view(B, S, n_kv_heads, head_dim).transpose(1, 2)
+    q_out, k_out = apply_rope(q, k, cos, sin)
+    k_out = torch.repeat_interleave(k_out, n_heads//n_kv_heads, dim=1)
+    v = torch.repeat_interleave(v, n_heads//n_kv_heads, dim=1)
+    x = F.scaled_dot_product_attention(q_out, k_out, v, is_causal=True)
+    x = x.transpose(1, 2).reshape(B, S, config.hidden_size)
+    return F.linear(x, o_w)
+
+def swiglu(x, weights, layer_idx):
+    g_w = weights[f"model.layers.{layer_idx}.mlp.gate_proj.weight"]
+    u_w = weights[f"model.layers.{layer_idx}.mlp.up_proj.weight"]
+    d_w = weights[f"model.layers.{layer_idx}.mlp.down_proj.weight"]
+    return F.linear(F.silu(F.linear(x, g_w)) * F.linear(x, u_w), d_w)
+
+def decoder_layer(x, weights, layer_idx, config, cos, sin):
+    input_layernorm_weight = weights[f"model.layers.{layer_idx}.input_layernorm.weight"]
+    post_attention_layernorm_weight = weights[f"model.layers.{layer_idx}.post_attention_layernorm.weight"]
+    eps = config.rms_norm_eps
+    h = x + gqa_attention(rmsnorm(x, input_layernorm_weight, eps), weights, layer_idx, config, cos, sin)
+    out = h + swiglu(rmsnorm(h, post_attention_layernorm_weight, eps), weights, layer_idx)
+    return out
+
+@torch.inference_mode()
+def forward(token_ids, weights, config):
+    B, S = token_ids.shape
+    embedding = F.embedding(token_ids, weights["model.embed_tokens.weight"])
+    cos, sin = rope(config.head_dim, S, config.rope_theta, device=embedding.device)
+    x = embedding
+    for i in range(config.num_hidden_layers):
+        x = decoder_layer(x, weights, i, config, cos, sin)
+    x = rmsnorm(x, weights["model.norm.weight"], config.rms_norm_eps)
+    return F.linear(x, weights["model.embed_tokens.weight"])
+
